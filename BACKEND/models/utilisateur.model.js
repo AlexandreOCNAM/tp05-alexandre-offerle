2733560a08d50bca462bc0009ddd2ab5bae69074
Alexandre <aofferle@cfai-formation.fr>
2023-11-23 16:18:07 +0100

    bbbbbbbbbbb

---
+++ BACKEND/models/utilisateur.model.js
@@ -0,0 +1,43 @@
+module.exports = (sequelize, Sequelize) => {
+    return sequelize.define("utilisateur", {
+
+    id: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        allowNull: false
+    },
+    nom: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    prenom: {
+        type: Sequelize.STRING
+        // allowNull defaults to true
+    },
+    adresse: {
+        type: Sequelize.STRING,
+    },
+    codepostal: {
+        type: Sequelize.STRING,
+    },
+    ville: {
+        type: Sequelize.STRING,
+    },
+    email: {
+        type: Sequelize.STRING,
+    },
+    sexe: {
+        type: Sequelize.STRING,
+    },
+    login: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+    },
+    telephone: {
+        type: Sequelize.STRING,
+    },
+});
+};
